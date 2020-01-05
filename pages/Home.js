37cc02c41268d84251ef40ace30b0ab82793b715
Ryan Xu <rxu@brandeis.edu>
2020-01-05 23:02:46 +0800

    Change to port 8080

--- pages/Home.js
+++ pages/Home.js
@@ -19,7 +19,6 @@ import {Container, Row, Col, Jumbotron, Button,
 import Link from 'next/link';
 import '../style.css';
 import Axios from 'axios';
-import ProjectDisplay from './Projects';
 import Header from '../comps/Header';
  
 /**
@@ -82,6 +81,95 @@ const IntroDisplay = () => {
     );
 };
 
+const ProjectDisplay = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        if (projects.length == 0) {
+            getProjects();
+        }
+    });
+
+    //retrieve projects from server
+    const getProjects = async () => {
+        setProjects(
+            await Axios.get('/projects/')
+            .then(response => {
+                return response.data.map(project => new Object({ //possibly make project object later
+                        id: project._id.toString(),
+                        name: project.projectname,
+                        description: project.shortdescription,
+                        image: project.image
+                    })
+                );
+            })
+        )
+    };
+
+    //renders individual project card
+    const ProjectCard = (props) => {
+        const lift = () => {
+            let project = document.getElementById(props.project.id + "-card");
+            if (project){
+                project.classList.toggle("card-shadow");
+            }
+        }
+
+        return (
+            <div>
+                <Link href={'/Project?id=' + props.project.id}>
+                    <Card className="project-card" id={props.project.id + "-card"} onMouseEnter={lift} onMouseLeave={lift}>
+                        <CardImg top width="100%" src={props.project.image} />
+                        <CardBody>
+                        <CardTitle>{props.project.name}</CardTitle>
+                        </CardBody>
+                    </Card>
+                </Link>
+            </div>
+        );
+    }
+
+    //so people on other servers can see
+    var mockProject = (id) => {
+        return {
+            id: id,
+            name: "Mock Project",
+            description: "So people on other servers can see what it looks like.",
+            image: "https://www.netclipart.com/pp/m/179-1791305_1-6-meet-schedule-mark-your-calendar-icon.png",
+        }
+    }
+
+    return (
+        <div className="project-display">
+            <Row>
+                <Col className="col-12 col-lg-4 col-md-6 col-sm-6">
+                    <ProjectCard project={mockProject("test1")}/>
+                </Col>
+                <Col className="col-12 col-lg-4 col-md-6 col-sm-6">
+                    <ProjectCard project={mockProject("test2")}/>
+                </Col>
+
+                <Col className="col-12 col-lg-4 col-md-6 col-sm-6">
+                    <ProjectCard project={mockProject("test3")}/>
+                </Col>
+                <Col className="col-12 col-lg-4 col-md-6 col-sm-6">
+                    <ProjectCard project={mockProject("test4")}/>
+                </Col>
+                <Col className="col-12 col-lg-4 col-md-6 col-sm-6">
+                    <ProjectCard project={mockProject("test5")}/>
+                </Col>
+                {
+                    !projects ? <></> :
+                    projects.map(project => {
+                        return(<Col className="col-12 col-lg-4 col-md-6 col-sm-6">
+                            <ProjectCard project={project}/>
+                        </Col>);
+                    })
+                }
+            </Row>
+        </div>
+    );
+}
 
 //sign up form
 const SignUp = (props) => {
