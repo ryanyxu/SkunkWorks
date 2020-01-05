1a43d8e2f8bffad1984090116b37b21ae83c0295
Ryan Xu <rxu@brandeis.edu>
2020-01-06 01:27:43 +0800

    a

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ console.log("URI: " + uri);
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
+
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
