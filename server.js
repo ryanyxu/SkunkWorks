8344b2e74ebdeaf04713d5f874d793292501680f
Ryan Xu <rxu@brandeis.edu>
2020-01-05 21:35:52 +0800

    Switch to mLab server

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
 
+console.log("URI: " + uri);
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 
