211a2a679a9698e0a2d0287185824fc6aa4657b1
Ryan Xu <rxu@brandeis.edu>
2020-01-05 21:38:23 +0800

    nothing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+console.log(process.env.PORT);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
